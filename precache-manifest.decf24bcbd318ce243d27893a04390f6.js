f1d6339ff4aca9d919b03608fba30955c3000b7f
ZHENYU HU <feiyeyuji@gamil.com>
2019-02-10 21:27:50 +1100

    Updates

---
+++ precache-manifest.decf24bcbd318ce243d27893a04390f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c2e4e301dd175d87d29",
+    "url": "/youtubevideos/static/css/main.e8ecc438.chunk.css"
+  },
+  {
+    "revision": "9c2e4e301dd175d87d29",
+    "url": "/youtubevideos/static/js/main.9c2e4e30.chunk.js"
+  },
+  {
+    "revision": "855fb0b70d169a81dc35",
+    "url": "/youtubevideos/static/js/1.855fb0b7.chunk.js"
+  },
+  {
+    "revision": "38258589ee62837be827",
+    "url": "/youtubevideos/static/js/runtime~main.38258589.js"
+  },
+  {
+    "revision": "6ddab01f8f80404d13862cebb745d5b9",
+    "url": "/youtubevideos/index.html"
+  }
+];
